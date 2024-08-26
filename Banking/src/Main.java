90c20acb9895ecfab45beec2c7b7b45c101cb79e
Washy <165256670+WASHYDENCEMAVUNDUKE@users.noreply.github.com>
2024-08-26 23:29:42 +0200

    Add files via upload

---
+++ Banking/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of the Bank class
+        Bank myBankAccount = new Bank("MTW", 230198);
+        myBankAccount.display(); // Invoking the display method
+    }
+}
